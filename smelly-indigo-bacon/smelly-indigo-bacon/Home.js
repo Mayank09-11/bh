a89dc6d6da97fca0b1bfde68db7e2e4e2e897100
Mayank09-11 <73886619+Mayank09-11@users.noreply.github.com>
2023-10-18 20:17:48 +0530

    Add files via upload

---
+++ smelly-indigo-bacon/smelly-indigo-bacon/Home.js
@@ -0,0 +1,23 @@
+import React ,{component} from "react";
+import{View,Text,FlatList,StyleSheet,Alert,SafeAreaView} from "react-native";
+import {ListItem}from "react-native-elements";
+import axios from "axios";
+export default class HomeScreen extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      listData:[],
+      url:"http://localhost:5000/"
+    };
+  }
+  componentDidMount(){
+    this.getPlanets();
+
+  }
+  getPlanets=()=>{
+    const {url}=this.state;
+    axios
+    .get(url)
+    .then(respond)
+  }
+}
